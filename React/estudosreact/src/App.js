df8acdab25d9941e2de536a8be2eda7f092eec5a
yorkt1 <guilherme_oliveira18@estudante.sesisenai.org.br>
2024-11-04 17:08:47 -0300

    Navbaresponsiva

--- React/estudosreact/src/App.js
+++ React/estudosreact/src/App.js
@@ -1,13 +1,20 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Root from "./routes/root";
+const router = createBrowserRouter([
+  {
+    path: "/",
+   element: <Root />,
+  },
+]);
 
-import './App.css';
-
-function App() {
-  return (
-   <>
-   a
-   
-   </>
-  );
-}
-
-export default App;
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ estrutura/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './componentes/Nav';
+
+function App() {
+  return (
+   
+<>
+<Navbar/>
+</>
+  );
+}
+
+export default App;
