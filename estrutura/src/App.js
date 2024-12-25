8f52846be40cd5abe893b71519a35c6a90331a2c
yorkt1 <guilherme_oliveira18@estudante.sesisenai.org.br>
2024-12-25 09:24:40 -0300

    Treinando react

--- estrutura/src/App.js
+++ estrutura/src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './componentes/Nav';
 
+const user = {
+  name: 'gui'
+};
+
 function App() {
+  // Definindo o estado do contador no nível do componente
+  const [count, setCount] = useState(0);
+
+  function handclick() {
+    alert('test click');
+  }
+
+
+  function AdicionarNumero() {
+    setCount(count + 1);
+  }
+
   return (
-   
-<>
-<Navbar/>
-</>
+    <>
+      <Navbar />
+      <h1>{user.name}</h1>
+      <button onClick={handclick}>BOTÃO PARA ALERTAR</button>
+      <button onClick={AdicionarNumero}>Test quantas vezes vc clicou: {count}</button>
+    </>
   );
 }
 
